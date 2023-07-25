8f9fc58e1c24810309b663985a1c1ec6667f376c
Jordan Fontaine <jordanmfontaine@gmail.com>
2023-07-25 07:58:19 -0400

    updated quiz entity

--- src/quizzes/entities/quiz.entity.ts
+++ src/quizzes/entities/quiz.entity.ts
@@ -1,1 +1,44 @@
-export class Quiz {}
+import { Course } from 'src/courses/entities/course.entity';
+import { Question } from 'src/questions/entities/question.entity';
+import { Result } from 'src/results/entities/result.entity';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+  ManyToOne,
+} from 'typeorm';
+
+@Entity()
+export class Quiz {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    length: 50,
+  })
+  name: string;
+
+  @Column('text')
+  description: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  // Relationship with Course
+  @ManyToOne(() => Course, (course) => course.quizzes)
+  course: Course[];
+
+  // Relationship with Question
+  @OneToMany(() => Question, (question) => question.quiz)
+  questions: Question[];
+
+  // Relationship with Result
+  @OneToMany(() => Result, (result) => result.quiz)
+  results: Result[];
+}
