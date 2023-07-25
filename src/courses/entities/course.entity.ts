db59fcc78801968a79d55513a57b8b19a62c4cf4
Jordan Fontaine <jordanmfontaine@gmail.com>
2023-07-25 08:00:10 -0400

    updated course entity

--- src/courses/entities/course.entity.ts
+++ src/courses/entities/course.entity.ts
@@ -1,1 +1,33 @@
-export class Course {}
+import { Quiz } from 'src/quizzes/entities/quiz.entity';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+} from 'typeorm';
+
+@Entity()
+export class Course {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    length: 50,
+  })
+  name: string;
+
+  @Column('text')
+  description: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  // Relationship with Quiz
+  @OneToMany(() => Quiz, (quiz) => quiz.course)
+  quizzes: Quiz[];
+}
