fa5fbbcb27a606e64e88732f0131afb30d76f18e
Jordan Fontaine <jordanmfontaine@gmail.com>
2023-07-25 07:57:26 -0400

    updated user entity and created result relationship

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,6 +1,41 @@
+import { Result } from 'src/results/entities/result.entity';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+} from 'typeorm';
 
-
+@Entity()
 export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
 
+  @Column({
+    length: 50,
+    unique: true,
+  })
+  username: string;
 
+  @Column({
+    length: 50,
+  })
+  password: string;
+
+  @Column({
+    length: 100,
+  })
+  email: string;
+
+  @Column()
+  created_at: Date;
+
+  @Column()
+  updated_at: Date;
+
+  // Relationship with Result
+  @OneToMany(() => Result, (result) => result.user)
+  results: Result[];
 }
