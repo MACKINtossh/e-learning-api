d21cf7519731a0106ecbccb3e39d83dd40a58ef3
Jordan Fontaine <jordanmfontaine@gmail.com>
2023-07-25 22:05:51 -0400

    Connected to database and swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import * as fs from 'fs';
+
+// Check if .env.local exists, if so, load it. Otherwise, load .env
+const envFile = fs.existsSync('.env.local') ? '.env.local' : '.env';
+dotenv.config({ path: envFile });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Amesite e-learning API')
+    .setDescription('The API description for Amesite e-learning platform')
+    .setVersion('1.0')
+    .addTag('e-learning')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3002);
 }
 bootstrap();
