960bd563f10ed3bed2f9e91e596e6e0dcba11aa2
Jordan Fontaine <jordanmfontaine@gmail.com>
2023-07-25 23:20:55 -0400

    added quiz import and provider

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -3,11 +3,12 @@ import { CoursesService } from './courses.service';
 import { CoursesController } from './courses.controller';
 import { Course } from './entities/course.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
+import { Quiz } from 'src/quizzes/entities/quiz.entity';
+import { QuizzesService } from 'src/quizzes/quizzes.service';
 
 @Module({
-imports: [TypeOrmModule.forFeature([Course])],
+  imports: [TypeOrmModule.forFeature([Course, Quiz])],
   controllers: [CoursesController],
-  providers: [CoursesService]
+  providers: [CoursesService, QuizzesService],
 })
 export class CoursesModule {}
